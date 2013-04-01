4cb572441dfd179cb4ba177a7c0b0dbe82fe4e0d
Bari David Gloria Garcia <bdavid2290@gmail.com>
2013-04-01 10:48:25 -0500

    Login desplegable
    Agregue un js para crear un login desplegable, falta validarlo

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -8,7 +8,34 @@
   <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
   <link rel="stylesheet" href="<?php echo base_url(); ?>css/style.css" type="text/css" media="screen" charset="utf-8" />
    <!-- <link rel="stylesheet" href="<?php echo base_url(); ?>css/style2.css" type="text/css" media="screen" charset="utf-8" /> -->
-  
+  <link rel="stylesheet" href="<?php echo base_url(); ?>css/login.css" type="text/css" media="screen" charset="utf-8" />
+<script type="text/javascript" src="<?php echo base_url(); ?>js/login.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js?ver=1.4.2"></script>
+<script type="text/javascript">
+
+// Login Form
+
+$(function() {
+    var button = $('#loginButton');
+    var box = $('#loginBox');
+    var form = $('#loginForm');
+    button.removeAttr('href');
+    button.mouseup(function(login) {
+        box.toggle();
+        button.toggleClass('active');
+    });
+    form.mouseup(function() { 
+        return false;
+    });
+    $(this).mouseup(function(login) {
+        if(!($(login.target).parent('#loginButton').length > 0)) {
+            button.removeClass('active');
+            box.hide();
+        }
+    });
+});
+
+</script>
 </head>
 
 <body>
@@ -29,7 +56,31 @@
           <li><a href="<?php echo base_url(); ?>index.php/login/eventos">Eventos</a></li>
           <li><a href="<?php echo base_url(); ?>index.php/login/signup">Registrate</a></li>
 		  <li><a>Contactenos</a></li>
-          <li><a href="<?php echo base_url(); ?>index.php/login/index">Login</a></li>
+          <li>
+		  
+		 
+          <div id="loginContainer"> <a href="#" id="loginButton"><span>Login</span><em></em></a>
+      <div style="clear:both"></div>
+      <div id="loginBox">
+        <form id="loginForm" action="<?php echo base_url();?>index.php/login/validate_credentials" method="post" accept-charset="utf-8">
+          <fieldset id="body">
+            <fieldset>
+              <label for="email">Usuario</label>
+              <input type="text" name="username" id="email" />
+            </fieldset>
+            <fieldset>
+              <label for="password">Password</label>
+              <input type="password" name="password" id="password" />
+            </fieldset>
+            <input type="submit" id="login" value="Sign in" />
+          </fieldset>
+          <span ><a href="#" >Olvidaste tu password?</a></span>
+        </form>
+      </div>
+    </div>
+		  
+		  
+		  </li>
         </ul>
       </div>
     </div>
--- application/views/simplestyle_6/eventos.php
+++ application/views/simplestyle_6/eventos.php
@@ -7,6 +7,35 @@
   <meta name="keywords" content="website keywords, website keywords" />
   <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
   <link rel="stylesheet" href="<?php echo base_url(); ?>css/style.css" type="text/css" media="screen" charset="utf-8" />
+  
+  <link rel="stylesheet" href="<?php echo base_url(); ?>css/login.css" type="text/css" media="screen" charset="utf-8" />
+<script type="text/javascript" src="<?php echo base_url(); ?>js/login.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js?ver=1.4.2"></script>
+<script type="text/javascript">
+
+// Login Form
+
+$(function() {
+    var button = $('#loginButton');
+    var box = $('#loginBox');
+    var form = $('#loginForm');
+    button.removeAttr('href');
+    button.mouseup(function(login) {
+        box.toggle();
+        button.toggleClass('active');
+    });
+    form.mouseup(function() { 
+        return false;
+    });
+    $(this).mouseup(function(login) {
+        if(!($(login.target).parent('#loginButton').length > 0)) {
+            button.removeClass('active');
+            box.hide();
+        }
+    });
+});
+
+</script>
 </head>
 
 <body>
@@ -27,7 +56,29 @@
           <li class="selected"><a href="<?php echo base_url(); ?>index.php/login/eventos">Eventos</a></li>
           <li><a href="<?php echo base_url(); ?>index.php/login/signup">Registrate</a></li>
           <li><a href="#">Contactanos</a></li>
-          <li><a href="<?php echo base_url(); ?>index.php/login/index">Login</a></li>
+          <li>
+		    
+         <div id="loginContainer"> <a href="#" id="loginButton"><span>Login</span><em></em></a>
+      <div style="clear:both"></div>
+      <div id="loginBox">
+        <form id="loginForm" action="<?php echo base_url();?>index.php/login/validate_credentials" method="post" accept-charset="utf-8">
+          <fieldset id="body">
+            <fieldset>
+              <label for="email">Usuario</label>
+              <input type="text" name="username" id="email" />
+            </fieldset>
+            <fieldset>
+              <label for="password">Password</label>
+              <input type="password" name="password" id="password" />
+            </fieldset>
+            <input type="submit" id="login" value="Sign in" />
+          </fieldset>
+          <span ><a href="#" >Olvidaste tu password?</a></span>
+        </form>
+      </div>
+    </div>
+		  
+		  </li>
          
         </ul>
       </div>
