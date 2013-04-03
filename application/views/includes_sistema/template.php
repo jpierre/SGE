2fdd140492911668c39a02a7876e82d90d6973d4
JPierre Perez Tang <yampier123@hotmail.com>
2013-04-03 11:55:30 -0500

    Archivos sobrantes.

--- application/views/includes_sistema/template.php
+++
@@ -1,8 +0,0 @@
-
-<?php $this->load->view('includes_sistema/header');?>
-
-<?php $this->load->view('includes_sistema/menu_izq');?>
-
-<?php $this->load->view($main_content);?>
-
-<?php $this->load->view('includes_sistema/footer');?>
