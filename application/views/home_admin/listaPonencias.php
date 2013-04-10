e5ec28a592e116ca95f89d9889b35e2aa270e914
JPierre Perez Tang <yampier123@hotmail.com>
2013-04-10 11:18:43 -0500

    Mantener - interrumpido

---
+++ application/views/home_admin/listaPonencias.php
@@ -0,0 +1,94 @@
+<link href="<?php echo base_url(); ?>css/wufo/structure.css" rel="stylesheet">
+<link href="<?php echo base_url(); ?>css/wufo/form.css" rel="stylesheet">
+<link href="<?php echo base_url(); ?>css/wufo/theme.css" rel="stylesheet">
+<div class="content">
+
+
+<div id="container" class="ltr">
+
+<form id="form6" name="form6" action="<?php echo base_url();?>index.php/inscripcion/inscribir/registar_inscrip/" class="wufoo topLabel page" autocomplete="off" enctype="multipart/form-data" method="post" accept-charset="utf-8">
+<header id="header" class="info">
+<h2>Bienvenidos! Eventos FIA ! </h2>
+<div>SGE es un proyecto para el curso de DIS enfocado a las necesitades de nuestra facultad.</div>
+</header>
+
+<ul>
+<li id="foli280" class="notranslate section "></li>
+<!-- <div id="oculta1"> -->
+<li id="foli284" class="likert notranslate 
+col5
+ ">
+<table cellspacing="0">
+<caption id="title284">
+Productos Encontrados
+</caption>
+<thead>
+<tr>
+<td><strong>Codigo</strong></td>
+<td ><strong>Nombre del Producto</strong></td>
+<td ><strong>Cantidad</strong></td>
+<td ><strong>Marca</strong></td>
+<td ><strong>Descripción del Producto</strong></td>
+<td ><strong>Seleccion</strong></td>
+</tr>
+</thead>
+<tbody>
+<tr class="statement284">
+<td >ART00068</td>
+<td >Corrector T/Lapicero </td>
+<td >28</td>
+<td >Artesco</td>
+<td >Corrector para boligrafos tinta liquida y seca.</td>
+<td ><input type="checkbox"></td>
+</tr>
+<tr class="alt statement285">
+<td >ART00115</td>
+<td >Tinta p/ tampon AZUL</td>
+<td >24</td>
+<td >Artesco</td>
+<td >Tinta p/ utilizar sobre tampon y tampon mini.</td>
+<td ><input type="checkbox"></td>
+</tr>
+<tr class="statement286">
+<td >ART00269</td>
+<td >Sacagrapas MINI</td>
+<td >18</td>
+<td >Artesco</td>
+<td >Sin descripción</td>
+<td ><input type="checkbox"></td>
+</tr>
+<tr class="statement287"> 
+<td >FAB00164</td>
+<td >Plumones 045 X12UND</td>
+<td >24</td>
+<td >Faber Castell</td>
+<td >Sin descripción</td>
+<td ><input type="checkbox"></td>
+</tr>
+<tr class="statement288">
+<td >FAB00166</td>
+<td >Plumones 045 X20UND</td>
+<td >36</td>
+<td >Faber Castell</td>
+<td >Sin descripción</td>
+<td ><input type="checkbox"></td>
+</tr>
+</tbody>
+</table>
+</li>
+<li > <input type="button" value="Agrega a Lista de Requerimientos" onClick="location.href='lista_req2.html'" class="btTxt submit" 
+    />
+</li>
+<li id="foli457" class="notranslate section      ">
+<section>
+<h3 id="title457" style="color:#F00;">
+NOTA: Se deben seleccionar productos de una misma Marca (Proveedor).
+</h3>
+</section>
+</li>
+<!--</div> -->
+</ul>
+</form> 
+			
+			</div><!--container-->
+<!-- end .content --></div>
