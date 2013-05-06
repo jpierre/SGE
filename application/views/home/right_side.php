4ebb75d4aa0a36eaa970eb014a42874b26a67cee
Bari David Gloria Garcia <bdavid2290@gmail.com>
2013-05-06 16:36:56 -0500

    3

--- application/views/home/right_side.php
+++
@@ -1,3 +0,0 @@
-<div class="sidebar2">
-	<img src="<?php echo base_url(); ?>images/buho.jpg" border="0"/>     
-</div>
--- application/views/home_admin/home.php
+++
@@ -1,9 +0,0 @@
-<?php $this->load->view('home_admin/header');?>
-
-<?php $this->load->view('home_admin/left_side');?>
-
-<?php $this->load->view($main_content);?>
-
-<?php $this->load->view('home_admin/right_side');?>
-
-<?php $this->load->view('home_admin/footer');?>
