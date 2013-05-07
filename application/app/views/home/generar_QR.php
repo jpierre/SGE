be01425cfad10c0b6a8a2b46b0c00b052337bf70
Bari David Gloria Garcia <bdavid2290@gmail.com>
2013-05-07 14:24:22 -0500

    Validacion del Generar PDF

--- application/app/views/home/generar_QR.php
+++ application/app/views/home/generar_QR.php
@@ -6,30 +6,34 @@
 
 <div class="content">
   <div id="container" class="ltr">
+  <br />
+  <br />
     <h1>Estos datos han generado tu codigo QR!!</h1>
     <h3>Es necesario que guardes este codigo QR para poder registrar tu asistencia en las ponencias que asistas</h3>
+    <br />
+    <br />
     <table width="364" height="166" border="0">
       <tr>
-        <td>Nombre</td>
-        <td><input id="nom" type="text" value="<?php echo $this->session->userdata('nombres'); ?>" readonly></td>
+        <td width="144">Nombre :</td>
+        <td width="210"><input id="nom" type="text" value="<?php echo $this->session->userdata('nombres'); ?>" readonly></td>
       </tr>
       <tr>
-        <td>Apellido Paterno</td>
+        <td>Apellido Paterno :</td>
         <td><input id="apepat" type="text" value="<?php echo $this->session->userdata('apePat'); ?>" readonly/></td>
       </tr>
       <tr>
-        <td>Apellido Materno</td>
+        <td>Apellido Materno :</td>
         <td><input id="apemat" type="text" value="<?php echo $this->session->userdata('apeMat'); ?>" readonly /></td>
       </tr>
     
       <tr>
-        <td>DNI</td>
+        <td>DNI :</td>
         <td><input id="dni" type="text" value="<?php echo $this->session->userdata('idDNI'); ?>" readonly/></td>
       </tr>
       <?php if($this->session->userdata('estado')=='1' || $this->session->userdata('estado')=='2'){?> ?>
       
       <tr>
-        <td>Codigo de alumno</td>
+        <td>Codigo de alumno :</td>
         <td><input id="cod" type="text"  value="<?php echo $this->session->userdata('codigo'); ?>" readonly/></td>
       </tr>
       <?php } ?>
@@ -39,17 +43,21 @@
     
     <canvas id="micanvas"> </canvas>
     <fieldset>
-      <legend>Pulsa sobre el tipo de imagen y el resultado se generar&aacute; abajo</legend>
-      <button id="png">Generar PNG</button>
-      |
-      <button id="jpeg">Generar JPEG</button>
+      <legend>SELECCIONA EL TIPO DE IMAGEN Y EL RESULTADO SE GENERARA ABAJO</legend>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <button class="css3button2" id="png">Generar QR PNG</button>
+      
+      <button class="css3button2" id="jpeg">Generar QR JPEG</button>
       <?php if($this->session->userdata('estado')=='1' || $this->session->userdata('estado')=='2'){?>
-      <a href="javascript:generarPDF()">Generar PDF</a>
+      <a class="css3button" href="javascript:generarPDF()">GENERAR PDF</a>
      
       <?php }else{ ?>
-      <a href="javascript:generarPDF2()">Generar PDF</a> <b>Debes generar primero la imagen</b> <br/>
+      <a class="css3button" href="javascript:generarPDF2()">GENERAR PDF</a>  <br/>
       <?php } ?>
     </fieldset>
+    <br />
+    <br />
     <img src="" id="laimagen"/> </div>
   <!--container--> 
   <!-- end .content --></div>
