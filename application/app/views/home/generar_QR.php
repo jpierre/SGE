6cfbcea28b8d1c2b658c3b1cadf6ecd1df38c214
Bari David Gloria Garcia <bdavid2290@gmail.com>
2013-05-07 11:13:47 -0500

    .....

--- application/app/views/home/generar_QR.php
+++ application/app/views/home/generar_QR.php
@@ -6,55 +6,46 @@
 
 <div class="content">
   <div id="container" class="ltr">
-  <h1>Estos datos han generado tu codigo QR!!</h1>
-  <h3>Es necesario que guardes este codigo QR para poder registrar tu asistencia en las ponencias que asistas</h3>
-  <table width="336" border="1">
-                <tr>
-                  <td>Nombre</td>
-                  <td><input id="nom" type="text" value="<?php echo $this->session->userdata('nombres'); ?>" readonly></td>
-                </tr>
-                <tr>
-                  <td>Apellido Paterno</td>
-                  <td><input id="apepat" type="text" value="<?php echo $this->session->userdata('apePat'); ?>" readonly/></td>
-                </tr>
-                <tr>
-                  <td>Apellido Materno</td>
-                  <td><input id="apemat" type="text" value="<?php echo $this->session->userdata('apeMat'); ?>" readonly /></td>
-                </tr>
-                <tr>
-                  <td>DNI</td>
-                  <td><input id="dni" type="text" value="<?php echo $this->session->userdata('idDNI'); ?>" readonly/></td>
-                </tr>
-                <?php if($this->session->userdata('estado')=='1' || $this->session->userdata('estado')=='2'):?>
-                <tr>
-                  <td>Codigo de alumno</td>
-                  <td><input id="cod" type="text"  value="<?php echo $this->session->userdata('codigo'); ?>" readonly/></td>
-                </tr>
-                <?php endif ?>
-              </table>
-  
-  <div style="display:none" id="output"></div>
-<canvas id="micanvas"> </canvas>
-<fieldset>
-  <legend>Pulsa sobre el tipo de imagen y el resultado se generar&aacute; abajo</legend>
-  <button id="png">Generar PNG</button>
-  |
-  <button id="jpeg">Generar JPEG</button>
-	
-  
-  <a href="javascript:generarPDF()">Generar PDF</a>
-  <br/>
-</fieldset>
-<img src="" id="laimagen"/> 
-  
-  </div>
+    <h1>Estos datos han generado tu codigo QR!!</h1>
+    <h3>Es necesario que guardes este codigo QR para poder registrar tu asistencia en las ponencias que asistas</h3>
+    <table width="336" border="1">
+      <tr>
+        <td>Nombre</td>
+        <td><input id="nom" type="text" value="<?php echo $this->session->userdata('nombres'); ?>" readonly></td>
+      </tr>
+      <tr>
+        <td>Apellido Paterno</td>
+        <td><input id="apepat" type="text" value="<?php echo $this->session->userdata('apePat'); ?>" readonly/></td>
+      </tr>
+      <tr>
+        <td>Apellido Materno</td>
+        <td><input id="apemat" type="text" value="<?php echo $this->session->userdata('apeMat'); ?>" readonly /></td>
+      </tr>
+      <tr>
+        <td>DNI</td>
+        <td><input id="dni" type="text" value="<?php echo $this->session->userdata('idDNI'); ?>" readonly/></td>
+      </tr>
+      
+      <?php if($this->session->userdata('estado')=='1' || $this->session->userdata('estado')=='2'):?>
+      <tr>
+        <td>Codigo de alumno</td>
+        <td><input id="cod" type="text"  value="<?php echo $this->session->userdata('codigo'); ?>" readonly/></td>
+      </tr>
+      <?php endif ?>
+    </table>
+    <div style="display:none" id="output"></div>
+    
+    
+    <canvas id="micanvas"> </canvas>
+    <fieldset>
+      <legend>Pulsa sobre el tipo de imagen y el resultado se generar&aacute; abajo</legend>
+      <button id="png">Generar PNG</button>
+      |
+      <button id="jpeg">Generar JPEG</button>
+      <a href="javascript:generarPDF()">Generar PDF</a>Debes generar primero la imagen <br/>
+    </fieldset>
+    <img src="" id="laimagen"/> </div>
   <!--container--> 
   <!-- end .content --></div>
-  
-  
-  
-  
 <?php $this->load->view('home/right_side');?>
 <?php $this->load->view('home/footer');?>
-
-
--- application/app/views/home/header.php
+++ application/app/views/home/header.php
@@ -6,12 +6,10 @@
 <link rel="stylesheet" href="<?php echo base_url(); ?>css/css.css" type="text/css" media="screen" charset="utf-8" />
 <script type='text/javascript' src="<?php echo base_url(); ?>js/funciones.js" ></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>js/libs/base64.js"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>js/libs/sprintf.js"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>js/jspdf.js"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>js/jspdf.plugin.addimage.js"></script>
-
+<script type="text/javascript" src="<?php echo base_url(); ?>js/libs/sprintf.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>js/jspdf.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>js/jspdf.plugin.addimage.js"></script>
 <title>Untitled Document</title>
-
 <script type="text/javascript">
 
 function generarPDF() {
@@ -20,31 +18,46 @@ function generarPDF() {
 	var apepat=document.getElementById('apepat').value;
 	var apemat=document.getElementById('apemat').value;
 	var dni=document.getElementById('dni').value;
-	
+	var cod=document.getElementById('cod').value;
 	var imagen=document.getElementById("laimagen");
+	
+	
+	
 
 	var doc = new jsPDF();
-	doc.text(50,20, 'Universidad de San Martin de Porres')
+    doc.setFontSize(15);
+	doc.setFont("times");
+	
+	doc.setLineWidth(0.5);
+	doc.line(20, 25, 190, 25);
+	
+	doc.text(70,20, 'Universidad de San Martin de Porres')
+	
 	doc.text(20, 40, 'Nombre :' + " "+ nombre);
 	doc.text(20, 50, 'Apellido : ' + " " + apepat + " " +apemat);
 	doc.text(20, 60, 'DNI :' + " "+ dni);
-	doc.addImage(imagen,'JPEG', 50, 70, 50, 50);
-	
+	doc.text(20,70, 'Codigo :' + " "+ cod);
+	doc.addImage(imagen,'JPEG', 80, 80, 50, 50);
 	
 	// Output as Data URI
 	doc.output('datauri');
+	
 }
 
 
 </script>
 <style>
-.content { margin-right: -1px; } /* this 1px negative margin can be placed on any of the columns in this layout with the same corrective effect. */
-ul.nav a { zoom: 1; }  /* the zoom property gives IE the hasLayout trigger it needs to correct extra whiltespace between the links */
+.content {
+	margin-right: -1px;
+} /* this 1px negative margin can be placed on any of the columns in this layout with the same corrective effect. */
+ul.nav a {
+	zoom: 1;
+}  /* the zoom property gives IE the hasLayout trigger it needs to correct extra whiltespace between the links */
 </style>
-<![endif]--></head>
+<![endif]-->
+</head>
 
 <body onload="init()">
-
 <div class="container">
-  <div class="header"><img src="<?php echo base_url(); ?>images/cabecera.png" width="1000" height="144" border="0"/> 
-    <!-- end .header --></div>
+<div class="header"><img src="<?php echo base_url(); ?>images/cabecera.png" width="1000" height="144" border="0"/> 
+  <!-- end .header --></div>
