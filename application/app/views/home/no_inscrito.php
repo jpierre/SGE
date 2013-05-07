c91cfaee859d979e0e4cfcd8b75b4f1a5268156b
Jonathan Renatto Vilcarromero Giraldo <r.vilcarromero@gmail.com>
2013-05-07 10:09:18 -0500

    Ajuste de QR acabado

---
+++ application/app/views/home/no_inscrito.php
@@ -0,0 +1,30 @@
+<?php $this->load->view('home/header');?>
+<?php $this->load->view('home/left_side');?>
+<link href="<?php echo base_url(); ?>css/wufo/structure.css" rel="stylesheet">
+<link href="<?php echo base_url(); ?>css/wufo/form.css" rel="stylesheet">
+<link href="<?php echo base_url(); ?>css/wufo/theme.css" rel="stylesheet">
+
+<div class="content">
+  <div id="container" class="ltr">
+  
+  
+     
+  
+  <div style="color:#F00">
+  <br /><br />
+  <span><h2>Atencion:<br />
+   Primero debes inscribirte a un evento para generar tu codigo QR </h2></span><br /><br /><br /><br />
+   
+  </div>
+
+  
+  </div>
+  <!--container--> 
+  <!-- end .content --></div>
+  
+  
+  
+  
+<?php $this->load->view('home/right_side');?>
+<?php $this->load->view('home/footer');?>
+
