27f99957c6aaeca83203b257320d8dbc8e68196d
JPierre Perez Tang <yampier123@hotmail.com>
2013-05-15 11:33:00 -0500

    Login funcionando

--- application/app/views/inicio2/login.php
+++ application/app/views/inicio2/login.php
@@ -169,19 +169,19 @@
 	        							
 				<!-- form -->
 					<script type="text/javascript" src="<?php echo base_url(); ?>js/inicio2/form-validation.js"></script>
-					<form id="contactForm" action="#" method="post">
+					<form id="contactForm" action="<?php echo base_url();?>seguridad/login/validate_credentials" method="post">
 						<h2 class="heading">Ingresa tus datos</h2>
 						<table>
 						<tr>
 								<td><label>Usuario</label></td>
-								<td><input name="email"  id="email" type="text" class="form-poshytip" title="Ingresa tu usuario" /></td>
+								<td><input name="username"  type="text" class="form-poshytip" title="Ingresa tu usuario" /></td>
 						</tr>
 						<tr>					
 								<td><label>Password</label></td>
-								<td><input name="email"  id="email" type="text" class="form-poshytip" title="Ingresa tu password" /></td>
+								<td><input name="password" type="password" class="form-poshytip" title="Ingresa tu password" /></td>
 						<tr>
 						</table>
-							<p><input type="button" value="Entrar" name="submit" id="submit" /></p>
+							<p><input type="submit" name="submit" value="Entrar"/></p>
 						
 					</form>
 					<!-- ENDS form -->				
